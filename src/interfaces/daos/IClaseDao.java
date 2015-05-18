f11834bee3985c74e868156ea0d3f3299bce0945
juanmont <juanjose.montiel@hotmail.com>
2015-05-18 16:56:37 +0200

    cambios en la gestion de clases DaoClases

--- src/interfaces/daos/IClaseDao.java
+++ src/interfaces/daos/IClaseDao.java
@@ -7,8 +7,10 @@ import transfers.Clases;
 
 public interface IClaseDao {
 
+	List<Clases> findAll(Connection connection);
+	List<Clases> findByIndividualGrupo(Connection connection, String individual);
 	List<Clases> findByCategoria(Connection connection, String categoria);
 	int insertClase(Connection connection, Clases clase);
-	int borrarClase(Connection connection, String id);
+	int borrarClase(Connection connection, int id);
 	Clases findById(Connection connection, int id);
 }
